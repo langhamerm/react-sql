d66324774b42156b46b73cc8a7d99d87c80f9797
Mark Langhamer <langhamerm@gmail.com>
2021-01-04 14:51:50 -0600

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+export class App extends Component {
+	state = {
+		products: [],
+		product: {
+			name: "",
+			price: 0,
+		},
+	};
+
+	componentDidMount() {
+		this.getProducts();
+	}
+
+	getProducts = () => {
+		fetch("http://localhost:4000/products")
+			.then((response) => response.json())
+			.then((response) => {
+				this.setState({
+					products: response.data,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	addProduct = () => {
+		const { product } = this.state;
+		fetch(
+			`http://localhost:4000/products/add?name=${product.name}&price=${product.price}`
+		)
+			.then(this.getProducts)
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	deleteProduct = () => {
+		const { product } = this.state;
+		axios
+			.delete(`http://localhost:4000/products/${product.name}`)
+			.then(this.getProducts)
+			.catch((err) => {
+				console.log(err);
+			});
+  };
+  
+   updateProduct = (newPrice) => {
+    const { product } = this.state;
+    axios.post(`http://localhost:4000/products/${product.name}/${newPrice}`)
+    .then(this.getProducts)
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+
+	renderProduct = ({ product_id, name, price }) => <div key={product_id}>{name} | {price}</div>;
+	render() {
+		const { products, product } = this.state;
+		return (
+			<div>
+				<div>{products.map(this.renderProduct)}</div>
+				<div>
+					<input
+						onChange={(e) => {
+							this.setState({
+								product: { ...product, name: e.target.value },
+							});
+						}}
+						value={product.name}
+						type="text"
+					/>
+					<input
+						onChange={(e) => {
+							this.setState({
+								product: { ...product, price: e.target.value },
+							});
+						}}
+						value={product.price}
+						type="number"
+					/>
+				</div>
+				<button onClick={this.addProduct}>Add</button>
+
+				<form onSubmit={this.deleteProduct}>
+					<input
+						onChange={(e) => {
+							this.setState({
+								product: { ...product, name: e.target.value },
+							});
+						}}
+						type="text"
+						value={product.name}
+					/>
+					<button type="submit">Delete</button>
+				</form>
+        
+				<form onSubmit={this.updateProduct(product.price)}>
+					<input
+						onChange={(e) => {
+							this.setState({
+								product: { ...product, name: e.target.value },
+							});
+						}}
+						type="text"
+						value={product.name}
+					/>
+          					<input
+						onChange={(e) => {
+							this.setState({
+								product: { ...product, price: e.target.value },
+							});
+						}}
+						value={product.price}
+						type="number"
+					/>
+					<button type="submit">Update Price</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
